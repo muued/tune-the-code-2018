d3bb7213f2a09bc79d1c481b70275711063e89ff
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2018-11-27 16:53:32 +0100

    introduce multi-threaded printing

--- src/main/java/tune/the/code/v2/Print.java
+++ src/main/java/tune/the/code/v2/Print.java
@@ -3,7 +3,10 @@ package tune.the.code.v2;
 import system.PrintSystem;
 import utils.Utils;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author Sevket Goekay <goekay@dbis.rwth-aachen.de>
@@ -12,40 +15,78 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class Print implements Runnable {
 
-    public static final int WAIT_ON_PRINT_QUEUE_MS = 1000;
+    private static final int WAIT_ON_PRINT_QUEUE_MS = 1000;
+    private static final int QUEUE_CONSUMER_SIZE = Runtime.getRuntime().availableProcessors();
 
-    private final MessageQueue<PrintInputData> messageQueue;
-    private final AtomicInteger printCount = new AtomicInteger(0);
-    private boolean run = true;
+    private final List<PrintWorker> queueConsumers = new ArrayList<>();
+    private final CountDownLatch latch = new CountDownLatch(QUEUE_CONSUMER_SIZE);
 
     public Print() {
-        this.messageQueue = MessageQueue.getInstance(PrintInputData.class);
+        MessageQueue<PrintInputData> messageQueue = MessageQueue.getInstance(PrintInputData.class);
+
+        for (int i = 0; i < QUEUE_CONSUMER_SIZE; i++) {
+            queueConsumers.add(new PrintWorker(messageQueue, latch));
+        }
     }
 
     @Override
     public void run() {
-        while (run || messageQueue.hasElements()) {
-            PrintInputData element = messageQueue.pop();
-            if (element == null) {
-                System.out.println("Print system waiting " + WAIT_ON_PRINT_QUEUE_MS + " ms for next input");
-                Utils.sleep(WAIT_ON_PRINT_QUEUE_MS);
-            } else {
-                printInkassoConfirmation(element);
-            }
-        }
+        queueConsumers.forEach(PrintWorker::start);
     }
 
     public void stop() {
-        run = false;
+        queueConsumers.forEach(PrintWorker::stop);
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void printInkassoConfirmation(PrintInputData data) {
-        boolean success = PrintSystem.doPrintInkassoConfirmation(data.getBank().getName(), data.getBankAccount(), data.getContractPolicyHolderName());
+    private static class PrintWorker implements Runnable {
 
-        if (success) {
-            printCount.incrementAndGet();
-        } else {
-            System.err.println("Could not print confirmation for " + data.getBank().toString() + ", " + data.getBankAccount() + ", " + data.getContractPolicyHolderName());
+        private final AtomicBoolean running = new AtomicBoolean(false);
+        private final MessageQueue<PrintInputData> messageQueue;
+        private final CountDownLatch latch;
+
+        private Thread worker;
+
+        private PrintWorker(MessageQueue<PrintInputData> messageQueue, CountDownLatch latch) {
+            this.messageQueue = messageQueue;
+            this.latch = latch;
+        }
+
+        private void start() {
+            worker = new Thread(this);
+            worker.start();
+        }
+
+        private void stop() {
+            running.set(false);
+        }
+
+        @Override
+        public void run() {
+            running.set(true);
+            while (running.get() || messageQueue.hasElements()) {
+                PrintInputData element = messageQueue.pop();
+                if (element == null) {
+                    System.out.println("Print system waiting " + WAIT_ON_PRINT_QUEUE_MS + " ms for next input");
+                    Utils.sleep(WAIT_ON_PRINT_QUEUE_MS);
+                } else {
+                    printInkassoConfirmation(element);
+                }
+            }
+            latch.countDown();
+        }
+
+        private void printInkassoConfirmation(PrintInputData data) {
+            boolean success = PrintSystem.doPrintInkassoConfirmation(data.getBank().getName(), data.getBankAccount(), data.getContractPolicyHolderName());
+
+            if (!success) {
+                System.err.println("Could not print confirmation for " + data.getBank().toString() + ", " + data.getBankAccount() + ", " + data.getContractPolicyHolderName());
+            }
         }
     }
 }
